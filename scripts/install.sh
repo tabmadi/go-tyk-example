78e321a65f65d298d75b9bcf0c0775cc54b2a077
Tab <tabmadi@proton.me>
2026-05-15 17:47:45 +0400

    chore: replace moon with mise

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-# Installing Moonrepo proto https://moonrepo.dev/proto
-bash -c "$$(curl -fsSL https://moonrepo.dev/install/proto.sh)"
+# Installing mise https://mise.jdx.dev
+curl https://mise.run | sh
 
-# Installing all the other tools with the proto
-proto use
+# Activate mise for current shell session
+eval "$(~/.local/bin/mise activate bash)"
 
-# Running the Moon setup task at the end
-moon :setup
+# Installing all tools defined in mise.toml
+mise install
+
+# Running the setup task
+mise run setup
